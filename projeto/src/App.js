c17a46e462d287a908acf288deb6094cf884cd6c
SilviaFernandaPereira <142239758+SilviaFernandaPereira@users.noreply.github.com>
2023-10-31 00:45:13 +0000

    cronometro último commit

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -3,64 +3,26 @@ import './index.css'
 
 function App() {
 
-  const [miliseconds,setMiliseconds] = useState ('00');
+  const [miliseconds,setmiliseconds] = useState ('00');
   const [seconds,setseconds] = useState ('00');
   const [minutes,setminutes] = useState ('00');
-  const [ligado,setLigado] = useState (false);
+  // const [ligado,setLigado] = useState (false);
 
-  if (ligado){
-    setTimeout(() => {
-      miliseconds();
-    }, 1);
-  }
-  function miliseconds(){
-    setMiliseconds(miliseconds + 1);
-   
-    if(miliseconds > 999){
-      setMiliseconds(0);
-      setseconds(seconds + 1);
-    }
-    if (seconds > 59){
-      setseconds(0);
-      setminutes(minutes + 1);
-    }
-  }
-  function reset(){
-    setseconds(0); 
-    setMiliseconds(0);
-    setminutes(0);
-  }
-  function pause(){
-    if (ligado){
-      setLigado(false)
-    } 
-
-    else if(!ligado) {
-      if (milisecondseconds > 1){
-        setLigado(true)
-      }
-    }
-  }
-
-  function stopWhatch (){
-    
-  }
 
   return (
     <div className='cronometro'>
       <div className="timer">
-        <p>{`${minutes}:${seconds}:${milisecondseconds}`}</p>
+        <p>{`${minutes}:${seconds}:${miliseconds}`}</p>
       </div>
       <div className="control">
-        <button onClick={() => setLigado(true)}>Start</button>
-        <button onClick={pause}>Pause</button>
-        <button onClick={() => {setLigado(false);setTimeout(() => {reset()}, 100)}}>Reset</button>
-        <button onClick={stopWhatch}>StopWhatch</button>
-
+        <button onClick>Start</button>
+        <button onClick>Pause</button>
+        <button onClick>Reset</button>
+        <button onClick>StopWhatch</button>
       </div>
-      
     </div>
   );
 }
 
 export default App;
+  
